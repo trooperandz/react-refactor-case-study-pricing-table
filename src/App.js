d0662e86d16d7b1ab509afb638d75a8d563b5f70
Matt Holland <mtholland10@gmail.com>
2018-09-18 13:41:56 -0500

    Initial commit; add html test file, add PricingContainer React test file, add styles

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PricingContainer from './PricingContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <PricingContainer />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
